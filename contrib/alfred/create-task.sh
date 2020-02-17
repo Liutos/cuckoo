26f904a6786a39e9c6997b36986d85ca55326787
Liutos <mat.liutos@gmail.com>
2020-02-17 10:37:28 +0800

    fix: 用引号包裹传递给JS脚本的参数，避免受空格影响。

--- contrib/alfred/create-task.sh
+++ contrib/alfred/create-task.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 负责创建延时提醒
 
-/usr/local/bin/node /Users/liutos/Projects/cuckoo/contrib/alfred/create-task.js ${1}
+/usr/local/bin/node /Users/liutos/Projects/cuckoo/contrib/alfred/create-task.js "${1}"
