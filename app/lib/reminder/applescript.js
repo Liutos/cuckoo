cdfe4a8b2bc3c5f34bb8f116bae86c0f34856745
Liutos <mat.liutos@gmail.com>
2020-04-02 11:36:36 +0800

    feat: 调整AppleScript弹出提醒时的文字

--- app/lib/reminder/applescript.js
+++ app/lib/reminder/applescript.js
@@ -1,15 +1,17 @@
 'use strict';
 
+const dateFormat = require('dateformat');
 const shell = require('shelljs');
 
 class AppleScriptReminder {
   notify(options) {
     const {
+      alarmAt,
       brief,
       detail,
       icon,
     } = options;
-    let command = `/usr/bin/osascript -e 'display notification "将会自动消失" with title "${brief}" subtitle "${detail}"'`;
+    let command = `/usr/bin/osascript -e 'display notification "预定弹出时间为${dateFormat(alarmAt * 1000, 'yyyy-mm-dd HH:MM:ss')}" with title "${brief}" subtitle "${detail}"'`;
     console.log('command', command);
     return new Promise(resolve => {
       shell.exec(command, { silent: true }, (code, stdout, stderr) => {
