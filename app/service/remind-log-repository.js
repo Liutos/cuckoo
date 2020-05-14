81b82d504cec66e02ff2b7771d0fa7859d732ab3
Liutos <mat.liutos@gmail.com>
2020-05-14 22:48:36 +0800

    refactor: Do the same thing on remind log entity.

---
+++ app/service/remind-log-repository.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class RemindLogService extends Service {
+  async create({ plan_alarm_at, real_alarm_at, task_id }) {
+    const { sqlite } = this.app;
+
+    await sqlite.run('INSERT INTO t_remind_log(create_at, plan_alarm_at, real_alarm_at, task_id, update_at) VALUES(?, ?, ?, ?, ?)', [
+      dateFormat(Date.now(), 'yyyy-mm-dd HH:MM:ss'),
+      plan_alarm_at,
+      real_alarm_at,
+      task_id,
+      dateFormat(Date.now(), 'yyyy-mm-dd HH:MM:ss'),
+    ]);
+  }
+
+  async search(query) {
+    const { sqlite } = this.app;
+
+    if (typeof query.sort !== 'string') {
+      query.sort = 'id:desc';
+    }
+
+    const columns = [];
+    const values = [];
+    if (typeof query.task_id === 'string') {
+      columns.push('task_id');
+      values.push(query.task_id);
+    }
+    let sql = 'SELECT * FROM t_remind_log';
+    if (columns.length > 0) {
+      sql += ' WHERE ' + columns.map(column => `${column} = ?`).join(' AND ');
+    }
+    sql += ` ORDER BY ${query.sort.split(':')[0]} ${query.sort.split(':')[1]}`;
+    sql += ' LIMIT ${query.limit || 10}';
+
+    return await sqlite.all(sql, values);
+  }
+}
+
+module.exports = RemindLogService;
--- app/service/remind-log.js
+++ app/service/remind-log.js
@@ -4,34 +4,36 @@ const Service = require('egg').Service;
 
 class RemindLogService extends Service {
   async create({ plan_alarm_at, real_alarm_at, task_id }) {
-    const { app } = this;
-    const { mysql } = app;
+    // const { app } = this;
+    // const { mysql } = app;
 
-    await mysql.insert('t_remind_log', {
-      create_at: new Date(),
-      plan_alarm_at,
-      real_alarm_at,
-      task_id,
-      update_at: new Date(),
-    });
+    // await mysql.insert('t_remind_log', {
+    //   create_at: new Date(),
+    //   plan_alarm_at,
+    //   real_alarm_at,
+    //   task_id,
+    //   update_at: new Date(),
+    // });
+    await this.ctx.service.remindLogRepository.create({ plan_alarm_at, real_alarm_at, task_id });
   }
 
   async search(query) {
-    const { mysql } = this.app;
+    // const { mysql } = this.app;
 
-    if (typeof query.sort !== 'string') {
-      query.sort = 'id:desc';
-    }
+    // if (typeof query.sort !== 'string') {
+    //   query.sort = 'id:desc';
+    // }
 
-    const where = {};
-    if (typeof query.task_id === 'string') {
-      where.task_id = query.task_id;
-    }
-    return await mysql.select('t_remind_log', {
-      limit: query.limit || 10,
-      orders: [ query.sort.split(':') ],
-      where,
-    });
+    // const where = {};
+    // if (typeof query.task_id === 'string') {
+    //   where.task_id = query.task_id;
+    // }
+    // return await mysql.select('t_remind_log', {
+    //   limit: query.limit || 10,
+    //   orders: [ query.sort.split(':') ],
+    //   where,
+    // });
+    return await this.ctx.service.remindLogRepository.search(query);
   }
 }
 
