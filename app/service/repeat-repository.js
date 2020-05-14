f11d4c27e4f0ec36b9374c9d1386d474dbaa4142
Liutos <mat.liutos@gmail.com>
2020-05-14 22:32:33 +0800

    refactor: Do the same thing on the repeat entity.

---
+++ app/service/repeat-repository.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Repeat = require('../lib/repeat');
+
+const Service = require('egg').Service;
+
+class RepeatService extends Service {
+  async create({ type }) {
+    const { sqlite } = this.app;
+
+    Repeat.validateType(type);
+    const result = await sqlite.run('INSERT INTO t_repeat(create_at, type, update_at) VALUES(?, ?, ?)', [
+      dateFormat(Date.now(), 'yyyy-mm-dd HH:MM:ss'),
+      type,
+      dateFormat(Date.now(), 'yyyy-mm-dd HH:MM:ss')
+    ]);
+    return await this.get(result.lastID);
+  }
+
+  async delete(id) {
+    const { logger, sqlite } = this.app;
+
+    await sqlite.run('DELETE FROM t_repeat WHERE id = ?', [id]);
+    logger.info(`删除t_repeat表中id列为${id}的行`);
+  }
+
+  async get(id) {
+    const { app, ctx } = this;
+    const { sqlite } = app;
+
+    const row = await sqlite.get('SELECT * FROM t_repeat WHERE id = ?', [id]);
+    return new Repeat(ctx, row);
+  }
+
+  async put(repeat) {
+    const { app } = this;
+    const { mysql } = app;
+
+    await mysql.run('UPDATE t_repeat SET type = ?, update_at = ? WHERE id = ?', [
+      repeat.type,
+      dateFormat(Date.now(), 'yyyy-mm-dd HH:MM:ss'),
+      repeat.id,
+    ]);
+  }
+}
+
+module.exports = RepeatService;
--- app/service/repeat.js
+++ app/service/repeat.js
@@ -6,25 +6,27 @@ const Service = require('egg').Service;
 
 class RepeatService extends Service {
   async create({ type }) {
-    const { app } = this;
-    const { mysql } = app;
+    // const { app } = this;
+    // const { mysql } = app;
 
-    Repeat.validateType(type);
-    const { insertId } = await mysql.insert('t_repeat', {
-      create_at: new Date(),
-      type,
-      update_at: new Date(),
-    });
-    return await this.get(insertId);
+    // Repeat.validateType(type);
+    // const { insertId } = await mysql.insert('t_repeat', {
+    //   create_at: new Date(),
+    //   type,
+    //   update_at: new Date(),
+    // });
+    // return await this.get(insertId);
+    return await this.ctx.service.repeatRepository.create({ type });
   }
 
   async delete(id) {
-    const { logger, mysql } = this.app;
+    // const { logger, mysql } = this.app;
 
-    await mysql.delete('t_repeat', {
-      id,
-    });
-    logger.info(`删除t_repeat表中id列为${id}的行`);
+    // await mysql.delete('t_repeat', {
+    //   id,
+    // });
+    // logger.info(`删除t_repeat表中id列为${id}的行`);
+    return await this.ctx.service.repeatRepository.delete(id);
   }
 
   async duplicate(repeat) {
@@ -32,27 +34,29 @@ class RepeatService extends Service {
   }
 
   async get(id) {
-    const { app, ctx } = this;
-    const { mysql } = app;
+    // const { app, ctx } = this;
+    // const { mysql } = app;
 
-    const row = await mysql.get('t_repeat', {
-      id,
-    });
-    return new Repeat(ctx, row);
+    // const row = await mysql.get('t_repeat', {
+    //   id,
+    // });
+    // return new Repeat(ctx, row);
+    return await this.ctx.service.repeatRepository.get(id);
   }
 
   async put(repeat) {
-    const { app } = this;
-    const { mysql } = app;
+    // const { app } = this;
+    // const { mysql } = app;
 
-    await mysql.update('t_repeat', {
-      type: repeat.type,
-      update_at: new Date(),
-    }, {
-      where: {
-        id: repeat.id,
-      },
-    });
+    // await mysql.update('t_repeat', {
+    //   type: repeat.type,
+    //   update_at: new Date(),
+    // }, {
+    //   where: {
+    //     id: repeat.id,
+    //   },
+    // });
+    return await this.ctx.service.repeatRepository.put(repeat);
   }
 }
 
