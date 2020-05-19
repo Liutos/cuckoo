656a2421361e59b1b36b682b1f5529d262fb8fba
Liutos <mat.liutos@gmail.com>
2020-05-19 21:33:53 +0800

    Add the SQL for creating tables in SQLite.

---
+++ sql/cuckoo-sqlite.sql
@@ -0,0 +1,54 @@
+CREATE TABLE t_context (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  create_at TEXT,
+  update_at TEXT
+);
+
+CREATE TABLE t_repeat (
+  id INTEGER PRIMARY KEY,
+  type TEXT,
+  create_at TEXT,
+  update_at TEXT
+);
+
+CREATE TABLE t_remind_log (
+  id INTEGER PRIMARY KEY,
+  plan_alarm_at INTEGER,
+  real_alarm_at INTEGER,
+  task_id INTEGER,
+  create_at TEXT,
+  update_at TEXT
+);
+
+CREATE TABLE t_remind (
+  id INTEGER PRIMARY KEY,
+  duration INTEGER,
+  repeat_id INTEGER,
+  restricted_hours INTEGER,
+  timestamp INTEGER,
+  create_at TEXT,
+  update_at TEXT
+);
+
+CREATE TABLE t_task (
+  id INTEGER PRIMARY KEY,
+  brief TEXT,
+  context_id INTEGER,
+  detail TEXT,
+  device TEXT,
+  icon TEXT,
+  icon_file TEXT,
+  remind_id INTEGER,
+  state TEXT,
+  create_at TEXT,
+  update_at TEXT
+);
+
+CREATE TABLE task_queue (
+  create_at INTEGER,
+  id INTEGER PRIMARY KEY,
+  next_trigger_time INTEGER,
+  task_id INTEGER,
+  update_at INTEGER
+);
