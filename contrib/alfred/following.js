d80113c9e708c2449d3948231278056070f86b47
Liutos <mat.liutos@gmail.com>
2020-06-05 23:02:18 +0800

    feat: Display date in the following list.

--- contrib/alfred/following.js
+++ contrib/alfred/following.js
@@ -18,24 +18,36 @@ async function main() {
     url
   });
   const { tasks } = response.body;
-  console.log(JSON.stringify({
-    items: tasks.map(({ plan_alarm_at, task }) => {
-      // subtitle展示的是任务下一次提醒的时刻，以及它的重复模式
-      let subtitle = dateFormat(plan_alarm_at * 1000, 'yyyy-mm-dd HH:MM:ss');
-      if (task.remind && task.remind.repeat) {
-        subtitle += ` *${task.remind.repeat.type}`;
-      }
+  const items = [];
+  let lastDate = null;
+  for (const t of tasks) {
+    const { plan_alarm_at, task } = t;
 
-      return {
-        arg: `${task.id}`,
-        icon: {
-          path: task.icon_file || ''
-        },
-        subtitle,
-        title: `#${task.id} ${task.brief} ${task.context ? '@' + task.context.name : ''}`
-      }
-    })
-  }, null, 2));
+    const currentDate = dateFormat(plan_alarm_at * 1000, 'yyyy-mm-dd');
+    if (!lastDate || lastDate !== currentDate) {
+      // 更新lastDate并写入items中，以作为下拉列表的一行显示。
+      lastDate = currentDate;
+      items.push({
+        icon: { path: '' },
+        title: `下列为${lastDate}的提醒`
+      });
+    }
+    // subtitle展示的是任务下一次提醒的时刻，以及它的重复模式
+    let subtitle = dateFormat(plan_alarm_at * 1000, 'yyyy-mm-dd HH:MM:ss');
+    if (task.remind && task.remind.repeat) {
+      subtitle += ` *${task.remind.repeat.type}`;
+    }
+
+    items.push({
+      arg: `${task.id}`,
+      icon: {
+        path: task.icon_file || ''
+      },
+      subtitle,
+      title: `#${task.id} ${task.brief} ${task.context ? '@' + task.context.name : ''}`
+    });
+  }
+  console.log(JSON.stringify({ items }, null, 2));
 }
 
 main();
