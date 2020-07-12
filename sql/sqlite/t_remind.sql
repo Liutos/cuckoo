e1db2385a5a7e4400cbe3e792ff902282efcbd2c
Liutos <mat.liutos@gmail.com>
2020-07-12 22:40:54 +0800

    Supplement the new column definitions.

--- sql/sqlite/t_remind.sql
+++ sql/sqlite/t_remind.sql
@@ -4,6 +4,7 @@ CREATE TABLE t_remind (
   repeat_id INTEGER,
   restricted_hours INTEGER,
   restricted_wdays INTEGER,
+  task_id INTEGER,
   timestamp INTEGER,
   create_at TEXT,
   update_at TEXT
--- sql/sqlite/task_queue.sql
+++ sql/sqlite/task_queue.sql
@@ -2,6 +2,7 @@ CREATE TABLE task_queue (
   create_at INTEGER,
   id INTEGER PRIMARY KEY,
   next_trigger_time INTEGER,
+  remind_id INTEGER,
   task_id INTEGER,
   update_at INTEGER
 );
