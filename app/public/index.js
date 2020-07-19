5607c14b12cc04eb399d0ec651446e999eb098ea
Liutos <mat.liutos@gmail.com>
2020-07-19 11:49:01 +0800

    feat: Re-write the Web UI for displaying tasks.

--- app/public/index.js
+++ app/public/index.js
@@ -1,105 +1,104 @@
+'use strict';
+
 const CUCKOO_PORT = 7001;
 
-const { tasks } = {
-  "tasks": []
-};
+const allTaskTemplate = `
+<table>
+  <tr>
+    <th>任务ID</th>
+    <th>任务简述</th>
+    <th>下一次提醒的时刻</th>
+    <th>场景要求</th>
+    <th>重复模式</th>
+  </tr>
+{{#each tasks}}
+  <tr>
+    <td>{{this.id}}</td>
+    <td>{{this.brief}}</td>
+    <td>{{this.remind.timestamp}}</td>
+    <td>
+    {{#if this.context}}
+      {{this.context.name}}
+    {{else}}
+      无要求
+    {{/if}}
+    </td>
+    <td>
+    {{#if this.remind.repeat}}
+      {{this.remind.repeat.type}}
+    {{else}}
+      不重复
+    {{/if}}
+    </td>
+  </tr>
+{{/each}}
+</table>
+`;
 
-Vue.component('task', {
-  methods: {
-    updateRemindTimestamp: function (event) {
-      const url = `http://localhost:${CUCKOO_PORT}/remind/${this.selected.remind.id}`;
-      console.log(`准备请求：${url}`);
-      axios
-        .patch(url, {
-          timestamp: parseInt(this.selectedRemindTimestamp)
-        }, {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        .then(response => {
-          console.log(`response`, response);
-          console.log(`将ID为${this.selected.id}的任务的提醒时刻修改为${this.selectedRemindTimestamp}`);
-        });
-    }
-  },
-  props: ['selected', 'selectedRemindTimestamp'],
-  template: '<div id="selected" v-if="selected">\n<p>简述：{{ selected.brief }}</p>\n<p>详情：{{ selected.detail }}</p>\n<p>下一个提醒时刻：<input v-model="selectedRemindTimestamp" /></p>\n<a v-on:click="updateRemindTimestamp">点我更新提醒时刻</a>\n</div>'
-});
+/**
+ * 拉取所有任务并展示到页面上。
+ */
+async function fetchAllTaskAndShow() {
+  // 请求接口获取任务列表
+  const url = `http://localhost:${CUCKOO_PORT}/task`;
+  const response = await fetch(url);
+  const body = await response.json();
+  const { tasks } = body;
+  // 构造HTML插入到id为taskListContainer的div中去
+  // 需要呈现的内容有：任务ID、任务简述、下一次提醒的时刻、场景要求、重复模式。
+  const makeTable = Handlebars.compile(allTaskTemplate);
+  const tableHTML = makeTable({ tasks });
+  document.getElementById('wholeTaskContainer').innerHTML = tableHTML;
+}
 
-var app = new Vue({
-  data: {
-    selected: {
-      brief: 'HELLO WORLD'
-    },
-    selectedRemindTimestamp: null,
-    tasks: tasks.map(({ plan_alarm_at, task }) => {
-      return {
-        alarmAt: plan_alarm_at,
-        brief: task.brief,
-        contextName: task.context ? task.context.name : '',
-        id: task.id
-      }
-    })
-  },
-  el: '#app',
-  filters: {
-    formatAlarmAt(value) {
-      return moment(value).format('YYYY-MM-DD hh:mm');
-    }
-  },
-  methods: {
-    selectTask: function (event) {
-      const taskId = event.target.id;
-      axios
-        .get(`http://localhost:${CUCKOO_PORT}/task/${taskId}`)
-        .then(response => {
-          this.selected = response.data.task;
-          this.selectedRemindTimestamp = this.selected.remind ? this.selected.remind.timestamp : null;
-          console.log('task.selected', this.selected);
-        });
-    }
-  },
-  mounted() {
-    axios
-      .get(`http://localhost:${CUCKOO_PORT}/task?state=active`)
-      .then(response => {
-        console.log('response', response);
-        this.tasks = response.data.tasks;
-      })
-  }
-})
+async function main() {
+  // 请求接口获取任务列表
+  const url = `http://localhost:${CUCKOO_PORT}/task/following`;
+  const response = await fetch(url);
+  const body = await response.json();
+  const { tasks } = body;
+  console.log(tasks);
+  // 构造HTML插入到id为taskListContainer的div中去
+  // 需要呈现的内容有：任务ID、任务简述、下一次提醒的时刻、场景要求、重复模式。
+  const tableTemplate = `
+<table>
+  <tr>
+    <th>任务ID</th>
+    <th>任务简述</th>
+    <th>下一次提醒的时刻</th>
+    <th>场景要求</th>
+    <th>重复模式</th>
+  </tr>
+{{#each tasks}}
+  <tr>
+    <td>{{this.task.id}}</td>
+    <td>{{this.task.brief}}</td>
+    <td>{{this.task.remind.timestamp}}</td>
+    <td>
+    {{#if this.task.context}}
+      {{this.task.context.name}}
+    {{else}}
+      无要求
+    {{/if}}
+    </td>
+    <td>
+    {{#if this.task.remind.repeat}}
+      {{this.task.remind.repeat.type}}
+    {{else}}
+      不重复
+    {{/if}}
+    </td>
+  </tr>
+{{/each}}
+</table>
+`;
+  const makeTable = Handlebars.compile(tableTemplate);
+  const tableHTML = makeTable({ tasks });
+  console.log('tableHTML', tableHTML);
+  document.getElementById('taskListContainer').innerHTML = tableHTML;
 
-window.createRemind = async function () {
-  // 通过ID取出提醒的时间戳，然后创建提醒。
-  let timestamp = document.getElementById('remindTimestamp').value;
-  timestamp = parseInt(timestamp);
-  if (!Number.isInteger(timestamp)) {
-    alert('提醒时刻必须是整数。');
-    return;
-  }
-  // 创建提醒
-  const response = await axios.post(`http://localhost:${CUCKOO_PORT}/remind`, {
-    timestamp
-  });
-  const { remind } = response.data;
-  console.log('remind', remind);
-  // 把提醒的ID填充到创建任务的表单中
-  document.getElementById('taskRemindId').value = remind.id;
-};
+  await fetchAllTaskAndShow();
+}
 
-window.createTask = async function () {
-  const brief = document.getElementById('taskBrief').value;
-  let remindId = document.getElementById('taskRemindId').value;
-  remindId = parseInt(remindId);
-  if (!Number.isInteger(remindId)) {
-    alert('请先创建提醒。');
-    return;
-  }
-  const response = await axios.post(`http://localhost:${CUCKOO_PORT}/task`, {
-    brief,
-    remind_id: remindId
-  });
-  const { task } = response.data;
-  console.log('任务创建完毕', task);
-};
+main();
+
