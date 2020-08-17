f911c366deb3f03f39100d93107e5804fed2803c
Liutos <mat.liutos@gmail.com>
2020-08-17 23:22:41 +0800

    test: A testcase for `service.remind.notify`.

---
+++ test/app/service/remind.test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { app, assert } = require('egg-mock/bootstrap');
+
+describe('test/app/service/remind.test.js', () => {
+  let remind1 = null;
+  let remind2 = null;
+
+  after(async () => {
+    const ctx = app.mockContext();
+    await ctx.service.remind.delete(remind1.id);
+    await ctx.service.remind.delete(remind2.id);
+  });
+
+  before(async () => {
+    const ctx = app.mockContext();
+    // 创建一个指定日期和一个指定小时的提醒
+    const restricted_hours = new Array(24);
+    restricted_hours.fill(0, 0, 24);
+    restricted_hours[(new Date().getHours() + 1) % 24] = 1;
+    remind1 = await ctx.service.remind.create({
+      restricted_hours,
+      timestamp: Date.now()
+    });
+    const restrictedWdays = new Array(7);
+    restrictedWdays.fill(0, 0, 7);
+    restrictedWdays[(new Date().getDay() + 1) % 7] = 1;
+    remind2 = await ctx.service.remind.create({
+      restrictedWdays,
+      timestamp: Date.now()
+    });
+  });
+
+  it('不在指定小时内不弹出', async function () {
+    const ctx = app.mockContext();
+    const result = await ctx.service.remind.notify(remind1, {});
+    assert(!result);
+  });
+
+  it('不在指定星期X内不弹出', async function () {
+    const ctx = app.mockContext();
+    const result = await ctx.service.remind.notify(remind2, {});
+    assert(!result);
+  });
+});
