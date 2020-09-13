9e19fa0d2962db150cbfb869e5e76546139b2b4f
Liutos <mat.liutos@gmail.com>
2020-09-13 11:05:27 +0800

    refactor: Exchange the order of creating remind and task.
    The `remind_id` column in table `t_task` will be removed in the
    future, the API's users(such as Alfred Workflow) must adjust its
    logics first.

--- contrib/alfred/create-task.js
+++ contrib/alfred/create-task.js
@@ -19,32 +19,12 @@ async function main() {
   if (type !== '') {
     repeat_type = type;
   }
-  // 先创建一个提醒
-  let timestamp = parseInt(query.timestamp);
-  if (Number.isNaN(timestamp)) {
-    const nMinutes = parseInt(query.delayMinutes);
-    timestamp = Math.round(new Date(Date.now() + nMinutes * 60 * 1000).setSeconds(0, 0) / 1000);
-  } else {
-    timestamp = Math.round(timestamp / 1000);
-  }
+  // 先创建一个任务
+  const brief = query.message;
   let response = await request({
     body: {
-      duration,
-      repeat_type,
-      timestamp
-    },
-    json: true,
-    method: 'post',
-    url: `${config.origin}/remind`
-  });
-  const { remind } = response.body;
-  // 再创建一个任务
-  const brief = query.message;
-  response = await request({
-    body: {
       brief,
-      device: device || null,
-      remind_id: remind.id
+      device: device || null
     },
     json: true,
     method: 'post',
@@ -52,6 +32,27 @@ async function main() {
   });
   const { task } = response.body;
   console.log(`创建了任务${task.id}`);
+  // 再创建一个提醒
+  let timestamp = parseInt(query.timestamp);
+  if (Number.isNaN(timestamp)) {
+    const nMinutes = parseInt(query.delayMinutes);
+    timestamp = Math.round(new Date(Date.now() + nMinutes * 60 * 1000).setSeconds(0, 0) / 1000);
+  } else {
+    timestamp = Math.round(timestamp / 1000);
+  }
+  response = await request({
+    body: {
+      duration,
+      repeat_type,
+      taskId: task.id,
+      timestamp
+    },
+    json: true,
+    method: 'post',
+    url: `${config.origin}/remind`
+  });
+  const { remind } = response.body;
+  console.log(`创建了提醒${remind.id}`);
 }
 
 main();
