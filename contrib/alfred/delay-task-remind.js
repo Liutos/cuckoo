3771cd479d271f4321911b4bd3bf20a57b2a4dfc
Liutos <mat.liutos@gmail.com>
2020-11-24 22:18:40 +0800

    refactor: Delay by specifying reminds' id.

--- contrib/alfred/delay-task-remind.js
+++ contrib/alfred/delay-task-remind.js
@@ -5,15 +5,12 @@ const config = require('./config');
 
 const request = require('co-request');
 
-/**
- * 根据ID获取指定任务
- */
-async function fetchTask(id) {
+async function fetchRemind(id) {
   const response = await request({
     json: true,
-    url: `${config.origin}/task/${id}`
+    url: `${config.origin}/remind/${id}`
   });
-  return response.body.task;
+  return response.body.remind;
 }
 
 /**
@@ -32,9 +29,9 @@ async function updateRemindTimestamp(id, timestamp) {
 
 async function main() {
   // 解析命令行参数
-  // 参数的顺序依次为：任务ID、推迟的时间。比如123 1d表示将ID为123的任务的提醒时间延迟1天。
+  // 参数的顺序依次为：提醒ID、推迟的时间。比如123 1d表示将ID为123的任务的提醒时间延迟1天。
   // 时间的单位支持：m（分钟）、h（小时）、d（天）。
-  const taskId = parseInt(process.argv[2], 10);
+  const remindId = parseInt(process.argv[2], 10);
   const interval = process.argv[3];
   const n = parseInt(interval.match(/[0-9]+/)[0]);
   const unit = interval[interval.length - 1];
@@ -47,9 +44,9 @@ async function main() {
     ns = n * 60;
   }
   // 查询这个任务
-  const task = await fetchTask(taskId);
+  const remind = await fetchRemind(remindId);
   // 提取出提醒的ID和预订的提醒时间
-  const { id: remindId, timestamp } = task.remind;
+  const { timestamp } = remind;
   // 修改提醒的触发时间
   await updateRemindTimestamp(remindId, timestamp + ns);
 }
--- contrib/alfred/pre-delay-task-remind.js
+++ contrib/alfred/pre-delay-task-remind.js
@@ -1,9 +1,32 @@
 /**
  * 说明将会如何推迟一个任务的提醒
  */
+const config = require('./config');
+
+const request = require('co-request');
+
+async function fetchRemind(id) {
+  const response = await request({
+    json: true,
+    url: `${config.origin}/remind/${id}`
+  });
+  return response.body.remind;
+}
+
+/**
+ * 根据ID获取指定任务
+ */
+async function fetchTask(id) {
+  const response = await request({
+    json: true,
+    url: `${config.origin}/task/${id}`
+  });
+  return response.body.task;
+}
+
 async function main() {
   // 解析命令行参数
-  // 参数的顺序依次为：任务ID、推迟的时间。比如123 1d表示将ID为123的任务的提醒时间延迟1天。
+  // 参数的顺序依次为：提醒ID、推迟的时间。比如123 1d表示将ID为123的任务的提醒时间延迟1天。
   // 时间的单位支持：m（分钟）、h（小时）、d（天）。
   if (process.argv.length < 4) {
     console.log(JSON.stringify({
@@ -20,7 +43,7 @@ async function main() {
     }, null, 2));
   }
 
-  const taskId = parseInt(process.argv[2], 10);
+  const remindId = parseInt(process.argv[2], 10);
   const interval = process.argv[3];
   const n = parseInt(interval.match(/[0-9]+/)[0]);
   const unit = interval[interval.length - 1];
@@ -33,15 +56,17 @@ async function main() {
     label = '分钟';
   }
   // 修改提醒的触发时间
+  const remind = await fetchRemind(remindId);
+  const task = await fetchTask(remind.taskId);
   console.log(JSON.stringify({
     items: [
       {
-        arg: `${taskId} ${interval}`,
+        arg: `${remindId} ${interval}`,
         icon: {
-          path: ''
+          path: task.icon_file
         },
-        subtitle: '',
-        title: `推迟任务${taskId} ${n}${label}`
+        subtitle: `任务简述：${task.brief}`,
+        title: `推迟任务${remind.taskId} ${n}${label}`
       },
     ]
   }, null, 2));
